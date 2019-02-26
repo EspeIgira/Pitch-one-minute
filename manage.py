b60c7357d2dc03ea1fb654f0ea7abbd9e068b14e
EspeIgira <Mapapuro2>
2019-02-26 16:33:42 +0200

    Three classes created

---
+++ manage.py
@@ -0,0 +1,17 @@
+from app import create_app
+from app import create_app,db
+
+
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+    
+if __name__ == '__main__':
+    manager.run()
