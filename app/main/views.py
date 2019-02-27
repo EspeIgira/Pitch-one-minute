d688518e391b2c5fdd746cf906bb3e76c9353dc8
EspeIgira <Mapapuro2>
2019-02-27 12:18:31 +0200

    able to display pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,5 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-# from ..request import get_movies,get_movie
-# from .forms import UserForm
 from flask_login import login_required
 from ..models import User
 
